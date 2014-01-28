6aed5ae1873eca34583b2e2c7f4bbbee5327cad4
Andrew Wu <arw@dmp.com.tw>
2014-01-28 17:56:06 +0800

    Check bison / flex.

--- build-djgpp.sh
+++ build-djgpp.sh
@@ -34,7 +34,7 @@ AUTOCONF_ARCHIVE="http://ftp.gnu.org/gnu/autoconf/autoconf-${AUTOCONF_VERSION}.t
 AUTOMAKE_ARCHIVE="http://ftp.gnu.org/gnu/automake/automake-${AUTOMAKE_VERSION}.tar.bz2"
 
 # check required programs
-REQ_PROG_LIST="gcc curl unzip"
+REQ_PROG_LIST="gcc curl unzip bison flex"
 
 for REQ_PROG in $REQ_PROG_LIST; do
   if ! which $REQ_PROG > /dev/null; then
