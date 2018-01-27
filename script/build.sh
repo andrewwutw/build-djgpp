andrewwutw/build-djgpp

commit 77be7730b85841390ddfde58a831768634b46ff1

add patch for binutils 2.27

script/build.sh
@@ -120,6 +120,9 @@ if [ ! -e ${DJGPP_PREFIX}/etc/binutils-${BINUTILS_VERSION}-installed ]; then
   unzip -o ../../download/bnu${BINUTILS_VERSION}s.zip || exit 1
   cd gnu/binutils-* || exit
   
+  # patch for binutils 2.27
+  ([ ${BINUTILS_VERSION} == 227 ] && patch bfd/init.c ../../../../patch/patch-bnu27-bfd-init.txt) || exit 1
+  
   # exec permission of some files are not set, fix it.
   for EXEC_FILE in install-sh missing; do
     echo "chmod a+x $EXEC_FILE"

commit 0136d82a07db54357b705399bfe53cbb8f35bac9

fixed bug introduced in 77be7730.

script/build.sh
@@ -121,7 +121,7 @@ if [ ! -e ${DJGPP_PREFIX}/i586-pc-msdosdjgpp/etc/binutils-${BINUTILS_VERSION}-in
   cd gnu/binutils-* || exit
   
   # patch for binutils 2.27
-  ([ ${BINUTILS_VERSION} == 227 ] && patch bfd/init.c ../../../../patch/patch-bnu27-bfd-init.txt) || exit 1
+  [ ${BINUTILS_VERSION} == 227 ] && ( patch bfd/init.c ../../../../patch/patch-bnu27-bfd-init.txt || exit 1 )
   
   # exec permission of some files are not set, fix it.
   for EXEC_FILE in install-sh missing; do