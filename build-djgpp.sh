e4502fc6fd6fe3e6a379407e4a19470f0e2c28ef
J.W. Jagersma <jwjagersma@gmail.com>
2018-02-03 04:51:15 +0100

    only install tar/sed if they haven't been installed previously.

--- build-djgpp.sh
+++ build-djgpp.sh
@@ -225,10 +225,13 @@ cd ${BASE}/build || exit 1
 # build GNU tar if needed.
 TAR=tar
 if [ ! -z $TAR_VERSION ]; then
-  echo "Building tar"
-  tar -xJvf ${BASE}/download/tar-${TAR_VERSION}.tar.xz || exit 1
-  cd tar-${TAR_VERSION}/
-  ./configure --prefix=${BASE}/build/tmpinst || exit 1
+  if [ ! -e ${BASE}/build/tmpinst/tar-${TAR_VERSION}-installed ]; then
+    echo "Building tar"
+    tar -xJvf ${BASE}/download/tar-${TAR_VERSION}.tar.xz || exit 1
+    cd tar-${TAR_VERSION}/
+    ./configure --prefix=${BASE}/build/tmpinst || exit 1
+    touch ${BASE}/build/tmpinst/tar-${TAR_VERSION}-installed
+  fi
   ${MAKE} -j${MAKE_JOBS} all install || exit 1
   TAR=${BASE}/build/tmpinst/bin/tar
 fi
@@ -243,10 +246,13 @@ cd ${BASE}/build || exit 1
 # build GNU sed if needed.
 SED=sed
 if [ ! -z $SED_VERSION ]; then
-  echo "Building sed"
-  untar sed-${SED_VERSION} || exit 1
-  cd sed-${SED_VERSION}/
-  ./configure --prefix=${BASE}/build/tmpinst || exit 1
+  if [ ! -e ${BASE}/build/tmpinst/sed-${SED_VERSION}-installed ]; then
+    echo "Building sed"
+    untar sed-${SED_VERSION} || exit 1
+    cd sed-${SED_VERSION}/
+    ./configure --prefix=${BASE}/build/tmpinst || exit 1
+    touch ${BASE}/build/tmpinst/sed-${SED_VERSION}-installed
+  fi
   ${MAKE} -j${MAKE_JOBS} all install || exit 1
   SED=${BASE}/build/tmpinst/bin/sed
 fi
