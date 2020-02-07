cda01e550f099959e8b0d2d71cb978e76d900805
J.W. Jagersma <jwjagersma@gmail.com>
2020-02-07 17:57:31 +0100

    clean up test script

--- script/test.sh
+++ script/test.sh
@@ -1,13 +1,6 @@
 #!/usr/bin/env bash
 set -e
 
-PREFIX="`pwd`/install"
-
-CFLAGS="-w"
-CXXFLAGS="-w"
-
-export PREFIX MAKE_JOBS CFLAGS CXXFLAGS
-
 case $TARGET in
 *-msdosdjgpp) SCRIPT=./build-djgpp.sh ;;
 ia16*)        SCRIPT=./build-ia16.sh ;;
@@ -15,4 +8,4 @@ avr)          SCRIPT=./build-avr.sh ;;
 *)            SCRIPT=./build-newlib.sh ;;
 esac
 
-echo | ${SCRIPT} ${PACKAGES}
+${SCRIPT} --batch --prefix=`pwd`/install --target=${TARGET} ${PACKAGES}
