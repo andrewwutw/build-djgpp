402ca5a44e3c747fb62d2daf3f2c7a574a05d866
J.W. Jagersma <jwjagersma@gmail.com>
2022-02-07 17:53:51 +0100

    sync ia16 configure flags with tkchia/build-ia16 (fixes #24)

--- build-ia16.sh
+++ build-ia16.sh
@@ -6,16 +6,30 @@ source script/init.sh
 TARGET="ia16-elf"
 
 prepend BINUTILS_CONFIGURE_OPTIONS "--disable-werror
-                                    --disable-nls"
+                                    --disable-nls
+                                    --disable-gdb
+                                    --disable-sim
+                                    --enable-x86-hpa-segelf=yes
+                                    --enable-ld=default
+                                    --enable-gold=yes"
 
 prepend GCC_CONFIGURE_OPTIONS "--disable-nls
-                               --enable-fat"
+                               --enable-fat
+                               --disable-libstdcxx-dual-abi
+                               --disable-extern-template
+                               --disable-wchar_t
+                               --disable-libstdcxx-verbose"
 
 prepend GDB_CONFIGURE_OPTIONS "--disable-werror
                                --disable-nls"
 
-prepend NEWLIB_CONFIGURE_OPTIONS "--enable-newlib-nano-malloc
-                                  --disable-newlib-multithread"
+prepend NEWLIB_CONFIGURE_OPTIONS "--enable-newlib-elix-level=2 --disable-elks-libc
+                                  --disable-newlib-wide-orient --enable-newlib-nano-malloc
+                                  --disable-newlib-multithread --enable-newlib-global-atexit
+                                  --enable-newlib-reent-small --disable-newlib-fseek-optimization
+                                  --disable-newlib-unbuf-stream-opt --enable-target-optspace
+                                  --enable-newlib-io-c99-formats --enable-newlib-mb --enable-newlib-iconv
+                                  --enable-newlib-iconv-encodings=utf_8,utf_16,cp850,cp852,koi8_uni"
 
 DEPS=""
 [ ! -z ${GCC_VERSION} ] && DEPS+=" newlib binutils"
